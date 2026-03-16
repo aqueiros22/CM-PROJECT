03d38248128e2ad59ed683eb20c76d2b3e1bed86
Gonçalo Moreira <gonc.moreira@gmail.com>
2026-03-16 16:16:04 +0000

    fix in database

--- app/src/main/java/com/example/fieldsense/data/AppDatabase.kt
+++ app/src/main/java/com/example/fieldsense/data/AppDatabase.kt
@@ -21,7 +21,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "fieldsense_database"
-                ).build()
+                ).fallbackToDestructiveMigration().build()
                 INSTANCE = instance
                 instance
             }
